f940cd017230b07415f20a3ca7f71cf866311e34
Max Nest <udomix.udomix@gmail.com>
2025-12-07 09:02:06 +0500

    add threading

--- Server/Server.hh
+++ Server/Server.hh
@@ -4,6 +4,14 @@
 
 #include <set>
 #include <csignal>
+#include <thread>
+#include <barrier>
+
+#ifdef WASM_SERVER
+#include <emscripten/proxying.h>
+#else
+#include <Loop.h>
+#endif
 
 class Client;
 
@@ -14,19 +22,24 @@ class WebSocketServer {
 public:
     WebSocketServer();
 };
-#else
-#include <App.h>
-typedef uWS::App WebSocketServer;
 #endif
 
 namespace Server {
-    extern uint8_t OUTGOING_PACKET[MAX_PACKET_LEN];
     extern std::array<GameInstance, Gamemode::kNumGamemodes> games;
-    extern std::array<uint32_t, PetalID::kNumPetals> petal_count_tracker;
+    extern std::array<std::atomic<uint32_t>, PetalID::kNumPetals> petal_count_tracker;
+    #ifdef WASM_SERVER
     extern WebSocketServer server;
+    #endif
     extern volatile sig_atomic_t is_draining;
     extern bool is_stopping;
-    extern uint32_t player_count;
+    extern std::atomic<uint32_t> player_count;
+    extern std::vector<std::thread> threads;
+    extern std::barrier<> barrier;
+    #ifdef WASM_SERVER
+    extern emscripten::ProxyingQueue queue;
+    #else
+    extern uWS::Loop *loop;
+    #endif
     extern void init();
     extern void run();
     extern void tick();
